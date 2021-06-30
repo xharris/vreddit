1d44490efe65739dca4987b5a0b4838f771e4cd1
xharris <xhh950@gmail.com>
2021-06-30 16:38:34 -0400

    adds arg parsing

--- vreddit.py
+++ vreddit.py
@@ -1,12 +1,20 @@
 # requires urllib, requests, ffmpeg executables
 
 CACHE = True
-PAGE_URL = 'https://www.reddit.com/r/leagueoflegends/comments/itx93a/chinese_fiora_king_01_second_4_vitals/'  # https://www.reddit.com/r/Subreddit/comments/asdf123/funnycatvideo
 OUTPUT = 'mp4'
 
 import urllib
 from pathlib import Path
-import re, subprocess, shutil, os, sys, requests, json
+import re, subprocess, shutil, os, sys, requests, json, argparse
+
+parser = argparse.ArgumentParser(description="Download a reddit video")
+parser.add_argument('url', type=str, nargs=1)
+parser.add_argument('-f', metavar='format', dest='format', default="mp4", type=str)
+args = parser.parse_args()
+
+print(args)
+PAGE_URL = args.url[0] # https://www.reddit.com/r/Subreddit/comments/asdf123/funnycatvideo
+OUTPUT = args.format 
 
 re_m3u8 = r'(HLS\w+\.m3u8)'
 re_reddit_src = r'v\.redd\.it\/(\w+)\/HLSPlaylist\.m3u8'
@@ -16,16 +24,11 @@ re_ts = r'(HLS\w+\.(ts|aac))'
 re_vid_ts = r'HLS_(\d+)[_MK]*\.(ts)'
 re_audio_ts = r'\w+AUDIO_(\d+)_K\.(aac)'
 
-# optional script args (overrides constants above)
-if len(sys.argv) >= 2:
-    PAGE_URL = sys.argv[1]
-if len(sys.argv) >= 3:
-    OUTPUT = sys.argv[2]
-
+def main():
+    go()
 
 def rel_path(*p):
     return os.path.join(os.path.dirname(__file__), *p)
-
 
 if len(PAGE_URL) == 0:
     exit('Error: Needs a page url')
@@ -54,138 +57,142 @@ def find_url_text(d):
     print(f"found {hash_result.group()}")
     return hash_result.group()
 
+def go():
+    # parse the url of the m3u8 playlist
+    m3u8_url = ''
+    with open('./cache.txt', 'a+') as f:
+        page_data = None
+        cache = {}
+        f.seek(0)
+        str_cache = f.read()
+        if len(str_cache) > 2:
+            cache = json.loads(str_cache)
 
-# parse the url of the m3u8 playlist
-m3u8_url = ''
-with open('./cache.txt', 'a+') as f:
-    page_data = None
-    cache = {}
-    f.seek(0)
-    str_cache = f.read()
-    if len(str_cache) > 2:
-        cache = json.loads(str_cache)
-
-    if CACHE and PAGE_URL in cache:
-        page_data = cache[PAGE_URL]
-    else:
-        req = requests.get(PAGE_URL + '.json',
-                           stream=True,
-                           headers={'User-agent': 'notabot'})
-
-        if req.content:
-            page_data = None
-            page_data = find_url_text(req.content)
-            # find_url(json.loads(req.content))
-
-            print(page_data)
+        if CACHE and PAGE_URL in cache:
+            page_data = cache[PAGE_URL]
         else:
+            req = requests.get(PAGE_URL + '.json',
+                            stream=True,
+                            headers={'User-agent': 'notabot'})
+
+            if req.content:
+                page_data = None
+                page_data = find_url_text(req.content)
+                # find_url(json.loads(req.content))
+
+                print(page_data)
+            else:
+                exit()
+
+        if not page_data:
+            print("No page data for thread url!")
             exit()
 
-    if not page_data:
-        print("No page data for thread url!")
-        exit()
+        f.seek(0)
+        cache[PAGE_URL] = page_data
+        f.truncate(0)
+        json.dump(cache, f)
 
-    f.seek(0)
-    cache[PAGE_URL] = page_data
-    f.truncate(0)
-    json.dump(cache, f)
+        m3u8_url = page_data
 
-    m3u8_url = page_data
+    # parse the name of the reddit post
+    vid_name = 'output'
+    vid_hash = ''
+    name_result = re.search(re_vid_name, PAGE_URL)
+    if name_result:
+        vid_name = name_result.group(1)
 
-# parse the name of the reddit post
-vid_name = 'output'
-vid_hash = ''
-name_result = re.search(re_vid_name, PAGE_URL)
-if name_result:
-    vid_name = name_result.group(1)
-
-# parse the id of the reddit post's m3u8 file
-hash_result = re.search(re_reddit_src, m3u8_url)
-if hash_result:
-    vid_hash = hash_result.group(1)
+    # parse the id of the reddit post's m3u8 file
+    hash_result = re.search(re_reddit_src, m3u8_url)
+    if hash_result:
+        vid_hash = hash_result.group(1)
 
 
-# used to download m3u8 and ts files of the reddit post
-def get_file(f):
-    print('download', f)
-    Path(rel_path("files")).mkdir(parents=True, exist_ok=True)
+    # used to download m3u8 and ts files of the reddit post
+    def get_file(f):
+        print('download', f)
+        Path(rel_path("files")).mkdir(parents=True, exist_ok=True)
 
-    url = 'https://v.redd.it/' + vid_hash + '/' + f
-    urllib.request.urlretrieve(url, rel_path('files', f))
+        url = 'https://v.redd.it/' + vid_hash + '/' + f
+        urllib.request.urlretrieve(url, rel_path('files', f))
 
 
-get_file('HLSPlaylist.m3u8')
+    get_file('HLSPlaylist.m3u8')
 
-# download other m3u8
-other_files = []
-with open(rel_path("files", "HLSPlaylist.m3u8"), 'r') as f:
-    lines = f.readlines()
-    for l in lines:
-        result = re.search(re_m3u8, l)
-        if result:
-            other_files.append(result.group(1))
-
-# download ts files found in m3u8 files
-ts_files = []
-highest_vid = 0
-highest_aud = 0
-highest_vid_f = None
-highest_aud_f = None
-for other in other_files:
-    get_file(other)
-    with open(rel_path('files', other), 'r') as f:
+    # download other m3u8
+    other_files = []
+    with open(rel_path("files", "HLSPlaylist.m3u8"), 'r') as f:
         lines = f.readlines()
         for l in lines:
-            result = re.search(re_ts, l)
+            result = re.search(re_m3u8, l)
             if result:
-                f_name = result.group(1)
-                ts_files.append(f_name)
+                other_files.append(result.group(1))
 
-                res_vid = re.search(re_vid_ts, f_name)
-                if res_vid:
-                    num = int(res_vid.group(1))
-                    if num > highest_vid:
-                        highest_vid_f = f_name
-                        highest_vid = num
-                res_aud = re.search(re_audio_ts, f_name)
-                if res_aud:
-                    num = int(res_aud.group(1))
-                    if num > highest_aud:
-                        highest_aud_f = f_name
-                        highest_aud = num
+    # download ts files found in m3u8 files
+    ts_files = []
+    highest_vid = 0
+    highest_aud = 0
+    highest_vid_f = None
+    highest_aud_f = None
+    for other in other_files:
+        get_file(other)
+        with open(rel_path('files', other), 'r') as f:
+            lines = f.readlines()
+            for l in lines:
+                result = re.search(re_ts, l)
+                if result:
+                    f_name = result.group(1)
+                    ts_files.append(f_name)
+
+                    res_vid = re.search(re_vid_ts, f_name)
+                    if res_vid:
+                        num = int(res_vid.group(1))
+                        if num > highest_vid:
+                            highest_vid_f = f_name
+                            highest_vid = num
+                    res_aud = re.search(re_audio_ts, f_name)
+                    if res_aud:
+                        num = int(res_aud.group(1))
+                        if num > highest_aud:
+                            highest_aud_f = f_name
+                            highest_aud = num
 
 
-def run_ffmpeg(cmd):
-    cmd = f"{rel_path('ffmpeg')} {cmd}"
-    print(cmd)
-    subprocess.run(cmd)
+    def run_ffmpeg(cmd):
+        cmd = f"{rel_path('ffmpeg')} {cmd}"
+        print(cmd)
+        subprocess.run(cmd)
 
 
-print('Audio: ' + str(highest_aud_f) + ', Video: ' + str(highest_vid_f))
+    print('Audio: ' + str(highest_aud_f) + ', Video: ' + str(highest_vid_f))
 
-mkv_path = rel_path('output.mkv')
-output_path = rel_path('output', vid_name + '.' + OUTPUT)
+    mkv_path = rel_path('output.mkv')
+    output_path = rel_path('output', vid_name + '.' + OUTPUT)
 
-if highest_vid_f:
-    Path(rel_path("output")).mkdir(parents=True, exist_ok=True)
+    if highest_vid_f:
+        Path(rel_path("output")).mkdir(parents=True, exist_ok=True)
 
-    get_file(highest_vid_f)
+        get_file(highest_vid_f)
 
-    # no audio cmd
-    cmd = f"-i {rel_path('files', highest_vid_f)} -map 0 -c copy {mkv_path}"
+        # no audio cmd
+        cmd = f"-i {rel_path('files', highest_vid_f)} -map 0 -c copy {mkv_path}"
 
-    # audio + video cmd
-    if highest_aud_f:
-        get_file(highest_aud_f)
-        cmd = f"-y -i {rel_path('files',highest_aud_f)} -r 30 -i {rel_path('files',highest_vid_f)} -filter:a aresample=async=1 -c:a flac -c:v copy {mkv_path} -crf 20"
+        # audio + video cmd
+        if highest_aud_f:
+            get_file(highest_aud_f)
+            cmd = f"-y -i {rel_path('files',highest_aud_f)} -r 30 -i {rel_path('files',highest_vid_f)} -filter:a aresample=async=1 -c:a flac -c:v copy {mkv_path} -crf 20"
 
-    run_ffmpeg(cmd)
+        run_ffmpeg(cmd)
 
-    # convert mkv to OUTPUT format
-    run_ffmpeg(f"-y -i {mkv_path} -crf 20 {output_path}")
+        # convert mkv to OUTPUT format
+        run_ffmpeg(f"-y -i {mkv_path} -crf 20 {output_path}")
 
-# clean up m3u8, ts, and aac files
-if os.path.exists(mkv_path):
-    os.remove(mkv_path)
-if os.path.exists(rel_path('files')):
-    shutil.rmtree(rel_path('files'))
+    # clean up m3u8, ts, and aac files
+    if os.path.exists(mkv_path):
+        os.remove(mkv_path)
+    if os.path.exists(rel_path('files')):
+        shutil.rmtree(rel_path('files'))
+
+    subprocess.Popen(['explorer', rel_path("output")])
+
+main()
